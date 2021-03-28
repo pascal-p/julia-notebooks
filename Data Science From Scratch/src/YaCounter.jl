4bcc247c4e3dfa4402b72dbc3e605eeceab1e6d4
Pascal P <lacsap_666@yahoo.fr>
2021-03-29 12:06:49 +1300

    add Counter struct.

---
+++ Data Science From Scratch/src/YaCounter.jl
@@ -0,0 +1,55 @@
+module YaCounter
+
+import Base: getindex, keys, values,
+  iterate, length, eltype
+
+export Counter, getindex, keys, values,
+  iterate, eltype
+
+const DT{T} = Dict{T, Integer} where T <: Any
+const VT{T} = Vector{T} where T <: Any
+
+struct Counter{T}
+  _hsh::DT
+  _nonkey::Bool   ## if true throws an exception when trying to access a non existent key
+  _defval::Integer
+
+  function Counter{T}(entry::VT{T};
+                      nokey_exception=true, defval=0) where T <: Any
+    counter_fn(entry) |> d -> new(d, nokey_exception, defval)
+  end
+end
+
+## API
+keys(self::Counter) = keys(self._hsh) |> collect
+
+values(self::Counter) = values(self._hsh) |> collect
+
+function getindex(self::Counter{T}, key::T) where T  <: Any
+  haskey(self._hsh, key) && (return self._hsh[key])
+  self._nonkey && throw(KeyError("No such key $(key) in Counter"))
+  self._defval  ## otherwise default value for non existent key (Int64)
+end
+
+function iterate(self::Counter{T}, state=(collect(self._hsh), 1)) where T
+  lst, ix = state
+  ix > length(self._hsh) && (return nothing)
+
+  (lst[ix], (lst, ix + 1))
+end
+
+length(self::Counter) = length(self._hsh)
+
+eltype(self::Counter{T}) where T = Pair{T, Int64}
+
+## internals
+
+function counter_fn(v::VT{T})::DT{T} where T
+  hcount = DT{T}()
+  for x ∈ v
+    hcount[x] = get(hcount, x, 0) + 1
+  end
+  hcount
+end
+
+end  ## module
---
+++ Data Science From Scratch/test/ya_counter_test.jl
@@ -0,0 +1,46 @@
+push!(LOAD_PATH, "./src")
+
+using Test
+using YaCounter
+
+@testset "Counter of String" begin
+  things = ["a", "bb" , "ac", "bb", "a", "b", "c"]
+  things_cnt = Counter{String}(things)
+
+  @test things_cnt["a"] == 2
+  @test things_cnt["bb"] == 2
+  @test things_cnt["b"] == 1
+  @test things_cnt["c"] == 1
+  
+  @test length(things_cnt) == 5
+  @test sort(values(things_cnt)) == [1, 1, 1, 2, 2]
+  
+  @test_throws KeyError things_cnt["Zoo"]
+end
+
+@testset "Counter of Integers" begin
+  things = [1, 2, 2, 2, 3, 4, 1, 2, 1, 4, 5, 1, 1, 0]
+  things_cnt = Counter{Int64}(things)
+
+  @test things_cnt[1] == 5
+  @test things_cnt[2] == 4
+  @test things_cnt[3] == 1
+  @test things_cnt[4] == 2
+  @test things_cnt[5] == 1
+  @test things_cnt[0] == 1
+  
+  @test length(things_cnt) == 6
+  @test sort(values(things_cnt)) == [1, 1, 1, 2, 4, 5]
+
+  @test eltype(things_cnt) == Pair{Int64, Int64}
+  
+  @test_throws KeyError things_cnt[70]
+end
+
+@testset "Counter of Integers / with no key exception" begin
+  things = [1, 2, 2, 2, 3, 4, 1, 2, 1, 4, 5, 1, 1, 0]
+  things_cnt = Counter{Int64}(things;
+                              nokey_exception=false, defval=-1)
+
+  @test things_cnt[70] == -1  ## that key does not exist
+end
