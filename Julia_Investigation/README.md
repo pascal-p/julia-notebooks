64da6a8e1ad2f87eabdb2cf6d2a11bbad48dd5df
Pascal P <lacsap_666@yahoo.fr>
2021-04-15 14:22:54 +1200

    investigation: move to Pluto nbs

---
+++ Julia_Investigation/README.md
@@ -0,0 +1,19 @@
+## Julia Investigation
+
+A collection of notebooks about learning (Julia Programming)[https://www.julialang.org/] programming language.<br />
+A good resource to start with `Julia` is [Julia Academy](https://juliaacademy.com/) and the excellent [Julia Documentation](https://docs.julialang.org/en/v1/)
+
+  - As of 2020, moved from `Jupyter` notebooks to `Pluto` notebooks
+
+
+<hr />
+
+  [The Julia Language](https://www.julialang.org/)
+
+<div align="right">
+    <img src="https://github.com/pascal-p/julia-notebooks/blob/main/Data%20Science%20From%20Scratch/src/julia-logo.svg" alt="JuliaLang Logo" width="100px" />
+</div>
+
+
+<hr />
+<p><sub><em>2019-2021 Corto Inc</sub></em></p>
--- Julia_Investigation/julia_explore_p09_functions_advanced.jl
+++ Julia_Investigation/julia_explore_p09_functions_advanced.jl
@@ -1,116 +1,320 @@
-### A Pluto.jl notebook ###
-# v0.14.1
-
-using Markdown
-using InteractiveUtils
-
-# ╔═╡ b8dda4e4-68e4-11eb-36ba-9df2d25c7737
-using PlutoUI
-
-# ╔═╡ 6e09faa6-68e6-11eb-3478-39a74c43259f
-using Test
-
-# ╔═╡ 4c32defe-68e4-11eb-2ccd-2b7bd29fbebd
-md"""
-### Functions in Julia / 2
-"""
-
-# ╔═╡ 676fac42-68e4-11eb-207b-19080677a358
-md"""
-##### Partial Application
-"""
-
-# ╔═╡ 43c74f81-2272-48df-b10a-fc6d345fe836
-PlutoUI.TableOfContents(indent=true, depth=4, aside=true)
-
-# ╔═╡ 75354800-68e4-11eb-1d71-79b19115a8ab
-function partial(fn::Function, x...)
-    # lamdba
-    (y...) -> fn(x..., y...)
-end
-
-# ╔═╡ 8dc62d9e-68e4-11eb-0d1d-61902cf5bd41
-adder(x::Integer, y::Integer) = x + y
-
-# ╔═╡ 9c57cbce-68e4-11eb-0617-5f9625aed520
-begin
-	add_2 = partial(adder, 2)
-	
-	with_terminal() do
-		println(typeof(add_2))
-	end
-end
-
-# ╔═╡ def5be8c-68e4-11eb-2186-bf8cf9dcaf2f
-add_2(4)
-
-# ╔═╡ ea11e87c-68e4-11eb-2bec-378c16cde8c3
-md"""
-#### Partial implemented as a macro
-"""
-
-# ╔═╡ f749b858-68e4-11eb-11a1-8bbdff30bcaa
-macro partial(fn, args...)
-	return :( (nargs...) -> $(esc(fn))($(esc(args))..., nargs...) )
-end
-
-# ╔═╡ 3dd12e5a-68e5-11eb-0fe6-858be3b23e04
-@macroexpand @partial(adder, 2)
-
-# ╔═╡ 6007a636-68e5-11eb-290d-7be1776c9b66
-padd2 = @partial(adder, 2)
-
-# ╔═╡ 2c8cc072-68e6-11eb-2f75-77e00bc75da3
-padd2(3)
-
-# ╔═╡ 407660a2-68e6-11eb-17b7-290309df8a69
-f(a, b, c, x, y, z) = √((x - a)^2 + (y - b)^2 + (z - c)^2)
-
-# ╔═╡ 4c60d834-68e6-11eb-0b4d-815b5ddec42f
-@macroexpand @partial(f, 1, 2, 3)
-
-# ╔═╡ 62934240-68e6-11eb-0da0-77ac10d3a66f
-f123 = @partial(f, 1, 2, 3)
-
-# ╔═╡ 7645d79c-68e6-11eb-0c4e-056ac6abb291
-@test f123(0, 0, 0) == √14
-
-# ╔═╡ 7f23d1cc-68e6-11eb-13fe-3b28848804ba
-@test f123(1, 2, 0) == 3
-
-# ╔═╡ 0562f2a6-0a70-440c-99b7-61e3441dcb96
-html"""
-<style>
-  main {
-        max-width: calc(800px + 25px + 6px);
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "## Functions in Julia"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "### Partial Application"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "_partial (generic function with 1 method)"
+      ]
+     },
+     "execution_count": 1,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "function _partial(f, x...)\n",
+    "    # lamdba\n",
+    "    (y...) -> f(x..., y...)\n",
+    "end"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "adder (generic function with 1 method)"
+      ]
+     },
+     "execution_count": 2,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "function adder(x::Int64, y::Int64)\n",
+    "    return x + y\n",
+    "end"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "#3 (generic function with 1 method)"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "_add2 = _partial(adder, 2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "getfield(Main, Symbol(\"##3#4\")){typeof(adder),Tuple{Int64}}"
+      ]
+     },
+     "execution_count": 4,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "typeof(_add2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "5"
+      ]
+     },
+     "execution_count": 5,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "_add2(3)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "### Partial implemented as a macro"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 6,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "@partial (macro with 1 method)"
+      ]
+     },
+     "execution_count": 6,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "macro partial(fun, arg1...)\n",
+    "    return :( (arg2...) -> $fun($arg1..., arg2...) )\n",
+    "end"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       ":(#1#arg2...->begin\n",
+       "          #= In[6]:2 =#\n",
+       "          Main.adder((2,)..., #1#arg2...)\n",
+       "      end)"
+      ]
+     },
+     "execution_count": 7,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "@macroexpand @partial(adder, 2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "#5 (generic function with 1 method)"
+      ]
+     },
+     "execution_count": 8,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "add2 = @partial(adder, 2)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "5"
+      ]
+     },
+     "execution_count": 9,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "add2(3)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "f (generic function with 1 method)"
+      ]
+     },
+     "execution_count": 10,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "f(a, b, c, x, y, z) = sqrt((x - a)^2 + (y - b)^2 + (z - c)^2)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 13,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       ":(#4#arg2...->begin\n",
+       "          #= In[6]:2 =#\n",
+       "          Main.f((1, 2, 3)..., #4#arg2...)\n",
+       "      end)"
+      ]
+     },
+     "execution_count": 13,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "@macroexpand @partial(f, 1, 2, 3)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 14,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "#9 (generic function with 1 method)"
+      ]
+     },
+     "execution_count": 14,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "f123 = @partial(f, 1, 2, 3)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "true"
+      ]
+     },
+     "execution_count": 18,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "f123(0, 0, 0) == √14"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
   }
-  .plutoui-toc.aside {
-    background: linen;
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Julia 1.5.3",
+   "language": "julia",
+   "name": "julia-1.5"
+  },
+  "language_info": {
+   "file_extension": ".jl",
+   "mimetype": "application/julia",
+   "name": "julia",
+   "version": "1.5.3"
   }
-  h3, h4 {
-        background: wheat;
-  }
-</style>
-"""
-
-# ╔═╡ Cell order:
-# ╟─4c32defe-68e4-11eb-2ccd-2b7bd29fbebd
-# ╟─676fac42-68e4-11eb-207b-19080677a358
-# ╠═b8dda4e4-68e4-11eb-36ba-9df2d25c7737
-# ╟─43c74f81-2272-48df-b10a-fc6d345fe836
-# ╠═75354800-68e4-11eb-1d71-79b19115a8ab
-# ╠═8dc62d9e-68e4-11eb-0d1d-61902cf5bd41
-# ╠═9c57cbce-68e4-11eb-0617-5f9625aed520
-# ╠═def5be8c-68e4-11eb-2186-bf8cf9dcaf2f
-# ╟─ea11e87c-68e4-11eb-2bec-378c16cde8c3
-# ╠═f749b858-68e4-11eb-11a1-8bbdff30bcaa
-# ╠═3dd12e5a-68e5-11eb-0fe6-858be3b23e04
-# ╠═6007a636-68e5-11eb-290d-7be1776c9b66
-# ╠═2c8cc072-68e6-11eb-2f75-77e00bc75da3
-# ╠═407660a2-68e6-11eb-17b7-290309df8a69
-# ╠═4c60d834-68e6-11eb-0b4d-815b5ddec42f
-# ╠═62934240-68e6-11eb-0da0-77ac10d3a66f
-# ╠═6e09faa6-68e6-11eb-3478-39a74c43259f
-# ╠═7645d79c-68e6-11eb-0c4e-056ac6abb291
-# ╠═7f23d1cc-68e6-11eb-13fe-3b28848804ba
-# ╟─0562f2a6-0a70-440c-99b7-61e3441dcb96
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
