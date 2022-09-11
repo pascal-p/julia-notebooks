10eee2b8edf4b9742ce7b0cbf32a9e984f70c7f0
Pascal P <lacsap_666@yahoo.fr>
2022-09-11 12:48:40 +1200

    micrograd - p3: auto backprop

--- Etudes/micrograd/micrograd.jl
+++ Etudes/micrograd/micrograd.jl
@@ -4,6 +4,12 @@
 using Markdown
 using InteractiveUtils
 
+# ╔═╡ bbd26197-1aaa-4869-9bf0-ea04326a0791
+begin
+	using PlutoUI
+	PlutoUI.TableOfContents(indent=true, depth=4, aside=true)
+end
+
 # ╔═╡ 4544523e-e72d-48ba-8418-655f397145b6
 using GraphViz, FileIO, Cairo
 
@@ -23,11 +29,17 @@ Links:
 
 """
 
+# ╔═╡ 99bc0941-b430-4e54-b355-63ef2fc6a124
+md"""
+### Data Structure
+"""
+
 # ╔═╡ 8c19aa91-77a7-46b1-b1ab-e31696924f15
 mutable struct Value{T <: Real}
 	data::T
 	_prev::Set
 	_op::Symbol
+	_backward::Function
 	label::String
 	grad::T
 	
@@ -37,8 +49,12 @@ mutable struct Value{T <: Real}
 		label::String=""
 	) where {T <: Real}
 		grad = zero(T)
-		new{T}(data, Set(_children), _op, label, grad)
+		_backward = () -> nothing # default to Nothing
+		new{T}(data, Set(_children), _op, _backward, label, grad)
 	end
+
+	# Value{T}(data::S) where {T <: Real, S <: Integer} = 
+	# 	Value{T}(T(data))
 end
 
 # ╔═╡ dbe5cd66-702b-40b0-8134-4d897a7cb05a
@@ -53,43 +69,36 @@ end
 # ╔═╡ 08edfe4c-c6d5-42ab-973c-34c34bf62f06
 import Base: +, -, *, /
 
-# ╔═╡ 53b65fae-2dca-4f2d-9aa0-388a3a127136
-Base.:+(self::Value{T}, other::Value{T}) where {T <: Real} = 
-	YaValue(self.data + other.data; _children=(self, other), _op=:+)
-
-# ╔═╡ e1592d55-a9c3-4da8-b295-79a2284c04c6
-Base.:-(self::Value{T}, other::Value{T}) where {T <: Real} = 
-	YaValue(self.data + other.data; _children=(self, other), _op=:+)
-
-# ╔═╡ b5983179-aeb3-4d38-a2fb-2b17522c98a3
-Base.:*(self::Value{T}, other::Value{T}) where {T <: Real} = 
-	YaValue(self.data * other.data; _children=(self, other), _op=:*)
-
 # ╔═╡ 11ae3bac-7f86-4435-a087-1eacbdd2945e
 Base.show(io::IO, self::Value) = print(io, "Value(data=$(self.data))")
 
-# ╔═╡ b3c4625f-29f3-48e7-927e-4951e0352c97
-function Base.tanh(self::Value{T}) where {T <: Real}
-	x = self.data
-	y = exp(2x)
-	YaValue((y - 1) / (y + 1); _children=(self, ), _op=:tanh, label="tanh")
+# ╔═╡ 8226ebf4-46eb-4ede-9a26-7dc29f2df38c
+function backward(self::Value{T}) where {T <: Real}
+	topo, visited = [], Set()
+	function build_topological_order(v::Value)
+		if v ∉ visited
+			push!(visited, v)
+			for child ∈ v._prev
+				build_topological_order(child)
+			end
+			push!(topo, v)
+		end
+	end
+	self.grad = 1.0
+	for cnode ∈ build_topological_order(self) |> reverse
+		cnode._backward()
+	end
 end
 
-# ╔═╡ 2b382d36-9ccf-4bd1-85b0-318946e6a039
-begin
-	a = YaValue(2.0; label="a") 
-	b = YaValue(-3.0; label="b")
-	c = YaValue(10.0; label="c")
+# ╔═╡ 66d05c88-d94a-49d4-8c26-871f7f49a2d8
+md"""
+--- 
+"""
 
-	d = a * b; d.label = "d"
-	e = d + c; e.label = "e"
-	
-	f = YaValue(-2.0; label="f")
-	L = e * f; L.label="Output"
-end
-
-# ╔═╡ c7830bbf-faf4-4994-a340-921167b351d5
-d._prev, d._op
+# ╔═╡ bdda1937-6719-4f90-b6b0-656e9b20bc12
+md"""
+### Visualization
+"""
 
 # ╔═╡ 102bd441-a350-4360-8ed0-612eae994f80
 begin
@@ -161,6 +170,166 @@ begin
 	end
 end
 
+# ╔═╡ 55e82545-c059-4c82-a301-71100f23b3d7
+md"""
+### Manual backpropagation and gradient 
+"""
+
+# ╔═╡ acf97b23-4113-420a-ad7d-cfaceba84569
+md"""
+Let's do backpropagation through tanh. So what is $\frac{do}{dn}$ given $o = tanh(n)$?
+
+By definition: $\frac{do}{dn} = 1 - o² = 1 - tanh(n)²$
+"""
+
+# ╔═╡ 5230e9a9-c8c4-4507-bbb4-9e549a8d6b48
+md"""
+### Backpropagation programmatically
+
+Of course, we need to update all arithmetic operations on our datatype. 
+"""
+
+# ╔═╡ 51450b56-72c5-496d-9d9b-fcc640b9b15e
+md"""
+Note: we can backpropagate given an order: the reverse of a topological order of the graph...
+"""
+
+# ╔═╡ 75a46369-3c75-4270-808e-70bb5de02f2e
+function topological_order(o::Value)
+	topo, visited = [], Set()
+	function build_topological_order(v::Value)
+		if v ∉ visited
+			push!(visited, v)
+			for child ∈ v._prev
+				build_topological_order(child)
+			end
+			push!(topo, v)
+		end
+	end
+	build_topological_order(o)
+end
+
+# ╔═╡ 98e308e4-5f5b-4f75-aec6-d29eb2be6e3f
+md"""
+After defining the function `backward` on our datatype (`Value`) we can invoke it!
+
+Let's do this...
+"""
+
+# ╔═╡ 64696b32-a4f2-4601-985f-86b00906bbb1
+md"""
+### Re-implementing tanh using basic building blocks
+
+First we want to be able to write something like
+
+```Julia
+a = Value(2.0, label="a")
+a + 1  # MethodError: no method matching +(...)
+```
+
+As it is with our datatype, this is not working because 1 is not a Value it is just an integer.
+OK, so let's add some methods (in `Julia` terminology) for our arithmetic operators, namely by adding promotion rules.
+"""
+
+# ╔═╡ ce0922f6-2e91-482e-9db7-c0a7f127b075
+begin
+	import Base: promote_rule, convert
+
+	convert(::Type{Value{T}}, x::Real) where {T <: Real} = Value{T}(T(x))
+	promote_rule(::Type{Value{T}}, ::Type{S}) where {T <: Real, S <: Integer} = Value{T}
+	# these two allow: promote(xi, i) where xi is Value{Int64} and i is Int64 => Value{Int64} 
+
+	# convert(::Type{Value{T}}, x::Type{Value{S}}) {T <: Real, S <: T} = Value{T}(T(x))
+	
+	# promote_rule(::Type{Value{T}}, ::Type{S}) where {T <: Real, S <: Real} = Value{promote_type(T, S)}
+	# promote_rule(::Type{Value{T}}, ::Type{Value{S}}) where {T <: Real, S <: Integer} = Value{promote_type(T, S)}
+	# romote_rule(::Type{Value{T}}, ::Type{S}) where {T <: Real, S <: Integer} = promote_type(T, S)
+
+end
+
+# ╔═╡ 3e1f8cdb-9638-41d1-963e-2eb369f66440
+begin
+	xa = YaValue(2.0, label="a")
+	typeof(xa)
+end
+
+# ╔═╡ 9872b0f0-3c86-498b-9e91-dfb7387907b2
+begin
+	xi = YaValue(2, label="i")
+	typeof(xi)
+end
+
+# ╔═╡ 43b8b610-bfa5-47e4-8445-59170d9b9d71
+begin
+	i = 2
+	promote(xi, i)
+end
+
+# ╔═╡ 3b6fdeeb-7ee3-41a4-bc46-67c122da3320
+Base.:+(self::Value{T}, other::T) where {T <: Real} = 
+	+(self, Value{T}(other))
+
+# ╔═╡ b5983179-aeb3-4d38-a2fb-2b17522c98a3
+function Base.:*(self::Value{T}, other::Value{T}) where {T <: Real}
+	y = YaValue(self.data * other.data; _children=(self, other), _op=:*)
+	function _backward_fn()
+		self.grad += other.data * y.grad
+		other.grad += self.data * y.grad
+	end
+	y._backward = _backward_fn
+	y
+end
+
+# ╔═╡ 53b65fae-2dca-4f2d-9aa0-388a3a127136
+function Base.:+(self::Value{T}, other::Value{T}) where {T <: Real}
+	y = YaValue(self.data + other.data; _children=(self, other), _op=:+)
+	function _backward_fn()
+		self.grad += 1.0 * y.grad
+		other.grad += 1.0 * y.grad
+	end
+	y._backward = _backward_fn
+	y
+end
+
+# ╔═╡ e1592d55-a9c3-4da8-b295-79a2284c04c6
+function Base.:-(self::Value{T}, other::Value{T}) where {T <: Real} 
+	y = YaValue(self.data + other.data; _children=(self, other), _op=:+)
+	function _backward_fn()
+		self.grad += 1.0 * y.grad
+		other.grad += 1.0 * y.grad
+	end
+	y._backward = _backward_fn
+	y
+end
+
+# ╔═╡ b3c4625f-29f3-48e7-927e-4951e0352c97
+function Base.tanh(self::Value{T}) where {T <: Real} 
+	x = exp(2*self.data)
+	tanh = (x - 1.) / (x + 1.)
+	y = YaValue(tanh; _children=(self, ), _op=:tanh, label="tanh")
+	function _backward_fn()
+		self.grad += (1. - tanh^2) * y.grad
+	end
+	y._backward = _backward_fn
+	y
+end
+
+# ╔═╡ 2b382d36-9ccf-4bd1-85b0-318946e6a039
+begin
+	a = YaValue(2.0; label="a") 
+	b = YaValue(-3.0; label="b")
+	c = YaValue(10.0; label="c")
+
+	d = a * b; d.label = "d"
+	e = d + c; e.label = "e"
+	
+	f = YaValue(-2.0; label="f")
+	L = e * f; L.label="Output"
+end
+
+# ╔═╡ c7830bbf-faf4-4994-a340-921167b351d5
+d._prev, d._op
+
 # ╔═╡ 3a83bb02-e9be-4102-a862-5e17abbd6f1c
 draw_dot(L)
 
@@ -226,13 +395,6 @@ begin
 	draw_dot(o)
 end
 
-# ╔═╡ acf97b23-4113-420a-ad7d-cfaceba84569
-md"""
-Let's do backpropagation through tanh. So what is $\frac{do}{dn}$ given $o = tanh(n)$?
-
-By definition: $\frac{do}{dn} = 1 - o² = 1 - tanh(n)²$
-"""
-
 # ╔═╡ 7e8a56cd-1cb0-40f0-a163-de0716b1fe02
 begin
 	n.grad = 1 - o.data^2
@@ -254,36 +416,137 @@ x₁.grad, w₁.grad = w₁.data * x₁w₁.grad, x₁.data * x₁w₁.grad
 # redraw graph with gradient updated
 draw_dot(o)
 
-# ╔═╡ 5230e9a9-c8c4-4507-bbb4-9e549a8d6b48
-# begin
-# 	# Hum interpolation does not seem to work - or rather I do not know how to make it working with
-# 	# string macro dot
-# 	# So here an alternative: create a string describing the graph and save it locally
-# 	# then load it
-# 	gr₁ = """
-# a [name="a",label="Foo",color=red,fontsize=8,shape=record,rankdir="LR"];
-# b [label="beta", shape=box];
-# a -- b -- c [color=blue,dir=forward];
-# """
-# 	gr₁ = string(gr₁, text"""b -- d [style=dotted,dir=forward];""")
-# 	gr₁ = string("""graph g {""", gr₁, """}""")
-
-# 	open("digraph1.dot", "w") do io
-# 		write(io, gr₁)
-# 	end
-
-# 	open("digraph1.dot", "r") do io
-# 		GraphViz.load(io)
-# 	end
-# end
+# ╔═╡ 251976c2-7609-4e6b-b26d-042651c01a06
+begin
+	(o₃, _rest_) = one_neuron() 
+	backward(o₃)
+	draw_dot(o₃)
+end
 
 # ╔═╡ 237f80c7-755e-450d-8b3b-8173914de65a
+function backprop_one_neuron()
+	# 2 inputs
+	x₁, x₂ = YaValue(2.0; label="x₁"), YaValue(0.0; label="x₂")
+	# 2 weights
+	w₁, w₂ = YaValue(-3.0; label="w₁"), YaValue(1.0; label="w₂")
+	# bias
+	b = YaValue(6.8813735870195432; label="b")
 
+	x₁w₁ = x₁ * w₁
+	x₁w₁.label = "x₁×w₁"
+	x₂w₂ = x₂ * w₂
+	x₂w₂.label = "x₂×w₂"
+
+	# x₁w₁ + x₂w₂ + b
+	x₁w₁x₂w₂ = x₁w₁ + x₂w₂
+	x₁w₁x₂w₂.label = "x₁×w₁ + x₂×w₂"
+	n = x₁w₁x₂w₂ + b
+	n.label = "n"
+
+	o = tanh(n)
+	o.label = "output"
+	# (o, n, x₁w₁x₂w₂, b, x₁w₁, x₂w₂, x₁, x₂, w₁, w₂)	
+	
+	# and now the backward pass
+	o.grad = 1.0
+	o._backward()
+	n._backward()
+	x₁w₁x₂w₂._backward()
+	x₁w₁._backward()
+	x₂w₂._backward()
+	# b._backward()
+	o
+end
 
 # ╔═╡ 4c71c0fa-801d-4f24-b385-b9ec6f275841
+begin
+	o₁ = backprop_one_neuron()
+	draw_dot(o₁)	
+end
+
+# ╔═╡ e99517de-a342-4c93-8237-a3ed4ed36fa9
+topological_order(o₁)
+
+# ╔═╡ bf1e7d64-d5b8-4d09-a4a9-2fca20abc05d
+function auto_backprop_one_neuron()
+	# 2 inputs
+	x₁, x₂ = YaValue(2.0; label="x₁"), YaValue(0.0; label="x₂")
+	# 2 weights
+	w₁, w₂ = YaValue(-3.0; label="w₁"), YaValue(1.0; label="w₂")
+	# bias
+	b = YaValue(6.8813735870195432; label="b")
+
+	# forward pass
+	x₁w₁ = x₁ * w₁
+	x₁w₁.label = "x₁×w₁"
+	x₂w₂ = x₂ * w₂
+	x₂w₂.label = "x₂×w₂"
+
+	# x₁w₁ + x₂w₂ + b
+	x₁w₁x₂w₂ = x₁w₁ + x₂w₂
+	x₁w₁x₂w₂.label = "x₁×w₁ + x₂×w₂"
+	n = x₁w₁x₂w₂ + b
+	n.label = "n"
+
+	o = tanh(n)
+	o.label = "output"
+	
+	
+	# and now the backward pass, using reverse order of the graph's topological order
+	o.grad = 1.0
+	for cnode ∈ topological_order(o) |> reverse
+		cnode._backward()
+	end
+	o
+end
+
+# ╔═╡ 0d9b8cf7-459e-4c99-9c26-a1030e12c821
+begin
+	o₂ = auto_backprop_one_neuron()
+	draw_dot(o₂)	
+end
+
+# ╔═╡ d4bbe45f-a7d9-4bcd-b163-b8d50138a017
+begin
+	# need to use += in _backward() function on Value type.
+	aa = YaValue(-2.0, label="a")
+	bb = aa + aa 
+	bb.label = "b"
+	backward(bb)
+	draw_dot(bb)  # double arrow from a to :+ - expected ∇(aa) == 2.  
+end
+
+# ╔═╡ ed9e2edb-4052-4a3d-8b7c-5b5bdd42a1d1
+xi + i
+
+# ╔═╡ 6b111617-410c-4a02-8a62-eb42545536a9
+# Value{Float64}(2)
+
+# ╔═╡ c42d26bb-0922-4332-acc5-0308ee06b709
+#         Value{Float64}   Int64 =>  Value{Float64}
+# Base.:+(self::Value{T}, other::S) where {T <: Real, S <: T} = 
+# 	+(self, Value{T}(other)) # +(self, Value{T}(T(other)))
+
+# ╔═╡ f6dbefe7-1073-41b3-a1cb-72d02a66bea0
+# xa + i
+
+# ╔═╡ c021c6bf-8ce8-416f-a22c-35f1045ed9c6
+# Base.:+(self::Value{T}, other::Value{S}) where {T <: Real, S <: T} = 
+#  	+(self, Value{T}(other))
+
+# ╔═╡ 296a8bdb-1a49-4ecf-a17f-1cf6971968ba
+# xa + xi
+
+# ╔═╡ d2e13f1c-02cf-4367-b802-990cfab7f272
 
 
-# ╔═╡ 8b6b20d7-d4ea-4c01-99f5-1fa51d7b24a2
+# ╔═╡ a833e0f8-7d31-4886-84da-0df97fdb4eed
+
+
+# ╔═╡ b80d3ff1-4b8b-4729-8ad2-d1698e973d20
+
+
+# ╔═╡ 50c9073c-2763-44d9-90bf-c9a48c63aefc
 
 
 # ╔═╡ a5e92195-d95e-4723-9c80-8d690517d1dd
@@ -301,12 +564,14 @@ PLUTO_PROJECT_TOML_CONTENTS = """
 Cairo = "159f3aea-2a34-519c-b102-8c37f9878175"
 FileIO = "5789e2e9-d7fb-5bc7-8068-2c6fae9b9549"
 GraphViz = "f526b714-d49f-11e8-06ff-31ed36ee7ee0"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 Printf = "de0858da-6303-5e67-8744-51eddeeeb8d7"
 
 [compat]
 Cairo = "~1.0.5"
 FileIO = "~1.15.0"
 GraphViz = "~0.2.0"
+PlutoUI = "~0.7.40"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -315,7 +580,13 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.8.0"
 manifest_format = "2.0"
-project_hash = "6322a56c369260bac57b83319542e88e043594f6"
+project_hash = "975543e91a3fb45489bc28fc776ac96900883e54"
+
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "8eaf9f1b4921132a4cff3f36a1d9ba923b14a481"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.1.4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
@@ -452,6 +723,24 @@ git-tree-sha1 = "129acf094d168394e80ee1dc4bc06ec835e510a3"
 uuid = "2e76f6c2-a576-52d4-95c1-20adfe4de566"
 version = "2.8.1+1"
 
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "8d511d5b81240fc8e6802386302675bdf47737b9"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.4"
+
+[[deps.HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "c47c5fa4c5308f27ccaac35504858d8914e102f9"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.4"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "f7be53659ab06ddc986428d3a9dcc95f6fa6705a"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.2"
+
 [[deps.InteractiveUtils]]
 deps = ["Markdown"]
 uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
@@ -461,6 +750,12 @@ deps = ["Preferences"]
 git-tree-sha1 = "abc9885a7ca2052a736a600f7fa66209f96506e1"
 uuid = "692b3bcd-3c85-4b1f-b108-f13ce0eb3210"
 version = "1.4.1"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "3c837543ddb02250ef42f4738347454f95079d4e"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.3"
 
 [[deps.LZO_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
@@ -542,6 +837,9 @@ deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
 version = "2.28.0+0"
 
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
 version = "2022.2.1"
@@ -578,6 +876,12 @@ git-tree-sha1 = "3a121dfbba67c94a5bec9dde613c3d0cbcf3a12b"
 uuid = "36c8627f-9965-5494-a995-c6b170f724f3"
 version = "1.50.3+0"
 
+[[deps.Parsers]]
+deps = ["Dates"]
+git-tree-sha1 = "3d5bf43e3e8b412656404ed9466f1dcbf7c50269"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.4.0"
+
 [[deps.Pixman_jll]]
 deps = ["Artifacts", "JLLWrappers", "Libdl", "Pkg"]
 git-tree-sha1 = "b4f5d02549a10e20780a24fce72bea96b6329e29"
@@ -588,6 +892,12 @@ version = "0.40.1+0"
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
 version = "1.8.0"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
+git-tree-sha1 = "a602d7b0babfca89005da04d89223b867b55319f"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.40"
 
 [[deps.Preferences]]
 deps = ["TOML"]
@@ -645,6 +955,15 @@ version = "1.0.0"
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
 version = "1.10.0"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[deps.Tricks]]
+git-tree-sha1 = "6bac775f2d42a611cdfcd1fb217ee719630c4175"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.6"
 
 [[deps.UUIDs]]
 deps = ["Random", "SHA"]
@@ -742,6 +1061,8 @@ version = "17.4.0+0"
 
 # ╔═╡ Cell order:
 # ╟─870398aa-30cc-11ed-3869-f5f7935be279
+# ╠═bbd26197-1aaa-4869-9bf0-ea04326a0791
+# ╟─99bc0941-b430-4e54-b355-63ef2fc6a124
 # ╠═8c19aa91-77a7-46b1-b1ab-e31696924f15
 # ╠═dbe5cd66-702b-40b0-8134-4d897a7cb05a
 # ╠═caa3d111-e372-4d1a-ad80-fe041e10d317
@@ -751,12 +1072,16 @@ version = "17.4.0+0"
 # ╠═b5983179-aeb3-4d38-a2fb-2b17522c98a3
 # ╠═11ae3bac-7f86-4435-a087-1eacbdd2945e
 # ╠═b3c4625f-29f3-48e7-927e-4951e0352c97
+# ╠═8226ebf4-46eb-4ede-9a26-7dc29f2df38c
+# ╟─66d05c88-d94a-49d4-8c26-871f7f49a2d8
 # ╠═2b382d36-9ccf-4bd1-85b0-318946e6a039
 # ╠═c7830bbf-faf4-4994-a340-921167b351d5
+# ╟─bdda1937-6719-4f90-b6b0-656e9b20bc12
 # ╠═4544523e-e72d-48ba-8418-655f397145b6
 # ╠═ab8703e5-e8e8-499d-b4ad-82b4160e4e0d
 # ╠═102bd441-a350-4360-8ed0-612eae994f80
 # ╠═3a83bb02-e9be-4102-a862-5e17abbd6f1c
+# ╟─55e82545-c059-4c82-a301-71100f23b3d7
 # ╠═f996e2ff-4d4e-45ce-83e1-bc9749e8cb32
 # ╠═93144f22-e9ce-4d7f-9ef7-7f223f435421
 # ╠═c4da7d5a-dba3-4242-bb5f-156cde691676
@@ -766,10 +1091,33 @@ version = "17.4.0+0"
 # ╠═3250073d-c4c1-4683-b182-edb953e10a45
 # ╠═7b7e521b-167a-4bbb-8ea6-c678c74cb557
 # ╠═a9697bad-9dd0-4b54-bed6-4a1f98245711
-# ╠═5230e9a9-c8c4-4507-bbb4-9e549a8d6b48
+# ╟─5230e9a9-c8c4-4507-bbb4-9e549a8d6b48
 # ╠═237f80c7-755e-450d-8b3b-8173914de65a
 # ╠═4c71c0fa-801d-4f24-b385-b9ec6f275841
-# ╠═8b6b20d7-d4ea-4c01-99f5-1fa51d7b24a2
+# ╟─51450b56-72c5-496d-9d9b-fcc640b9b15e
+# ╠═75a46369-3c75-4270-808e-70bb5de02f2e
+# ╠═e99517de-a342-4c93-8237-a3ed4ed36fa9
+# ╠═bf1e7d64-d5b8-4d09-a4a9-2fca20abc05d
+# ╠═0d9b8cf7-459e-4c99-9c26-a1030e12c821
+# ╟─98e308e4-5f5b-4f75-aec6-d29eb2be6e3f
+# ╠═251976c2-7609-4e6b-b26d-042651c01a06
+# ╠═d4bbe45f-a7d9-4bcd-b163-b8d50138a017
+# ╟─64696b32-a4f2-4601-985f-86b00906bbb1
+# ╠═ce0922f6-2e91-482e-9db7-c0a7f127b075
+# ╠═3e1f8cdb-9638-41d1-963e-2eb369f66440
+# ╠═9872b0f0-3c86-498b-9e91-dfb7387907b2
+# ╠═43b8b610-bfa5-47e4-8445-59170d9b9d71
+# ╠═3b6fdeeb-7ee3-41a4-bc46-67c122da3320
+# ╠═ed9e2edb-4052-4a3d-8b7c-5b5bdd42a1d1
+# ╠═6b111617-410c-4a02-8a62-eb42545536a9
+# ╠═c42d26bb-0922-4332-acc5-0308ee06b709
+# ╠═f6dbefe7-1073-41b3-a1cb-72d02a66bea0
+# ╠═c021c6bf-8ce8-416f-a22c-35f1045ed9c6
+# ╠═296a8bdb-1a49-4ecf-a17f-1cf6971968ba
+# ╠═d2e13f1c-02cf-4367-b802-990cfab7f272
+# ╠═a833e0f8-7d31-4886-84da-0df97fdb4eed
+# ╠═b80d3ff1-4b8b-4729-8ad2-d1698e973d20
+# ╠═50c9073c-2763-44d9-90bf-c9a48c63aefc
 # ╟─a5e92195-d95e-4723-9c80-8d690517d1dd
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
