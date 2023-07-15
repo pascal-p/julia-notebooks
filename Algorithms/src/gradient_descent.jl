2a0ba4d690cd825af72a65b75b681a149aa4b215
Pascal P <lacsap_666@yahoo.fr>
2023-07-16 10:33:41 +1200

    add SGD

--- Algorithms/src/gradient_descent.jl
+++ Algorithms/src/gradient_descent.jl
@@ -6,7 +6,7 @@ using LinearAlgebra
 """
     ∇_descent(𝒟train, φ, ∇loss; η=0.1, T=100)
 
-Perform stochastic gradient descent on a provided dataset `𝒟train`.
+Perform gradient descent (GD) on a provided dataset `𝒟train`.
 - `𝒟train`: The training dataset (X, y), X the matrix of input, y (a vector) representing the target (ground truth)
 - `φ`:       The initial point or starting parameter values
 - `∇loss`:   The gradient of the loss function to minimize
@@ -19,7 +19,7 @@ Note:
   "𝒟" can be typed by \\scrD<tab>
   "φ" can be typed by \\varphi<tab>
 """
-function ∇_descent(𝒟train, φ, ∇loss; η=0.1, N=100) # where T <: Number
+function ∇_descent(𝒟train, φ, ∇loss; η=0.1, N=100)
   𝐰 = (φ(𝒟train[1][1]) |> length |> rand) # starting from any point in the parameter space
   for _ ∈ 1:N
     𝐰 .-= η * mean(∇loss(X, y, 𝐰, φ) for (X, y) ∈ 𝒟train)
@@ -27,13 +27,25 @@ function ∇_descent(𝒟train, φ, ∇loss; η=0.1, N=100) # where T <: Number
   𝐰
 end
 
-# function ∇_descent(𝒟train::AbstractVector{Tuple{T, T}}, φ, ∇loss; η=0.1, N=100) where T <: Number
+"""
+    stochastic_∇_descent(𝒟train, φ, ∇loss; η=0.1, T=100)
 
-# function ∇_descent(𝒟train::AbstractVector{Tuple{Vector{T}, T}}, φ, ∇loss; η=0.1, N=100) where T <: Number
-#   𝐰 = φ(𝒟train[1][1]) |> length |> rand
-#   for _ ∈ 1:N
-#     𝐰 .-= η * mean(∇loss(X, y, 𝐰, φ) for (X, y) ∈ 𝒟train)
-#   end
-#   𝐰
-# end
+Perform stochastic gradient descent (SGD) on a provided dataset `𝒟train`.
+- `𝒟train`: The training dataset (X, y), X the matrix of input, y (a vector) representing the target (ground truth)
+- `φ`:       The initial point or starting parameter values
+- `∇loss`:   The gradient of the loss function to minimize
+- `η`:       The learning rate (default 0.1)
+- `N`:       The max number of iterations (default 100)
 
+The function will return the updated values for φ.
+"""
+function stochastic_∇_descent(𝒟train, φ, ∇loss; η=0.1, N=500)
+  𝐰 = (φ(𝒟train[1][1]) |> length |> rand) # starting from any point in the parameter space
+  for _ ∈ 1:N
+    # make an update for each datapoint (the key difference between SGD and GD)
+    for (X, y) ∈ 𝒟train
+      𝐰 .-= η * ∇loss(X, y, 𝐰, φ)
+    end
+  end
+  𝐰
+end
--- Algorithms/test/test_gradient_descent.jl
+++ Algorithms/test/test_gradient_descent.jl
@@ -1,5 +1,6 @@
 using Statistics
 using LinearAlgebra
+using Random
 using Test
 
 include("../src/gradient_descent.jl")
@@ -17,10 +18,12 @@ include("../src/gradient_descent.jl")
 
   idₓ = x -> x
 
+  𝒟train₀ = [([3., 0.7], 4.), ([-1., 0.3], 3.), ([-1., -3.], 0.)]
+
   """
   Single-dimensional training input data.
   """
-  function test_gradient_descent()
+  function test_∇_descent_basic()
     𝒟train = [(3, 4), (-1, 3), (-1, 0)]
     𝐰_opt = ∇_descent(𝒟train, idₓ, ∇loss_squared)
     y_opt = μ_loss(𝐰_opt, 𝒟train, idₓ, loss_squared)
@@ -28,21 +31,57 @@ include("../src/gradient_descent.jl")
   end
 
   """
-         Multi-dimensional training data input.
+  Multi-dimensional training data input.
   """
-  function test_gradient_descent_multi()
-    𝒟train = [([3., 0.7], 4.), ([-1., 0.3], 3.), ([-1., -3.], 0.)]
-    𝐰_opt = ∇_descent(𝒟train, idₓ, ∇loss_squared)
+  function test_∇_descent_multi_basic()
+
+    𝐰_opt = ∇_descent(𝒟train₀, idₓ, ∇loss_squared)
+    y_opt = μ_loss(𝐰_opt, 𝒟train₀, idₓ, loss_squared)
+    (𝐰_opt, y_opt)
+  end
+
+  """
+  (again) Multi-dimensional training data input.
+  """
+  function test_stochastic_∇_descent_basic()
+    𝐰_opt = stochastic_∇_descent(𝒟train₀, idₓ, ∇loss_squared; η=0.01)
+    y_opt = μ_loss(𝐰_opt, 𝒟train₀, idₓ, loss_squared)
+    (𝐰_opt, y_opt)
+  end
+
+  function test_stochastic_∇_descent(;η=0.01, N=1_000, seed=70)
+    Random.seed!(seed)
+    # Create random dataset with 100 rows and 5 columns
+    X = randn(100, 5)
+    # Create corresponding target value by adding random noise in the dataset
+    y = (idₓ.(X) * [5, 3, 1, 2, 4]) .+ randn(100) .* 0.1
+
+    𝒟train = [(X[ix, :], y[ix]) for ix ∈ 1:size(X, 1)]  # iterator
+    𝐰_opt = stochastic_∇_descent(𝒟train, idₓ, ∇loss_squared; η=η, N=N)
     y_opt = μ_loss(𝐰_opt, 𝒟train, idₓ, loss_squared)
     (𝐰_opt, y_opt)
   end
 
-  𝐰, y = test_gradient_descent()
+
+  𝐰, y = test_∇_descent_basic()
   @test 𝐰 ≈ [0.8181818181818182]
   @test y ≈ 5.878787878787879
 
 
-  𝐰, y = test_gradient_descent_multi()
+  𝐰, y = test_∇_descent_multi_basic()
   @test 𝐰 ≈ [0.8314306533883896, -0.03036191401505953]
   @test y ≈ 5.876487733786738
+
+
+  𝐰, y = test_stochastic_∇_descent_basic()
+  @test 𝐰 ≈ [0.8286254042639639, -0.07376670863696766]
+  @test y ≈ 5.8829223883616395
+
+  ϵ = 1.e-6
+  isapprox(a, b; rtol = ϵ, kwargs...) = Base.isapprox(a, b; rtol=rtol, kwargs...)
+
+
+  𝐰, y = test_stochastic_∇_descent(N=10_000)
+  @test 𝐰 ≈ [4.994195281768873, 2.9888364454480634, 0.9895028912070879, 1.998500451626844, 3.9940672737357628] # rtol = ϵ
+  @test y ≈ 0.009403064501924843 # rtol = ϵ
 end
