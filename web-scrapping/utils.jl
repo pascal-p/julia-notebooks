5081af01723a1d51afe9d696fb8103768fb05167
Pascal P <lacsap_666@yahoo.fr>
2024-01-17 20:42:31 +1300

    utils: add web links handling

--- web-scrapping/utils.jl
+++ web-scrapping/utils.jl
@@ -2,16 +2,19 @@
 
 # include("../../../NLP_LLM/Summarize_Papers_with_GPT/support/api_module.jl")
 
-const SYS_PROMPT = """You are a smart AI research assistant tasked with analyzing, summarizing, and synthesizing articles. You are thorough and deliberate in your approach before drafting your answers. You excel at organizing the provided articles into coherent sections, with introductions and conclusions that highlight the main ideas, reasoning, and contributions.
+const SYS_PROMPT = """```markdown
+You are a smart AI research assistant tasked with analyzing, and synthesizing articles. You are thorough and deliberate in your approach before drafting your answers. You excel at organizing the provided articles into coherent sections, with introductions and conclusions that highlight the main ideas, reasoning, and contributions.
 
-You proceed methodically, step by step, ensuring that the synthesis is accurately structured into coherent sections, capturing every fact, example, and subtlety. You always begin with a section detailing the article's title, publication date, author, and link (when such information is available). You keep all the original sections from the article for high fidelity. Moreover, you meticulously extract and render all pertinent external links and references (including but not limited to GitHub repositories) cited within the source article. In cases where the full details of these links or references are not available, you explicitly indicate their absence. Aim for a synthesis that is exhaustive, without overlooking any significant information.
+You proceed methodically, step by step, ensuring that the synthesis is accurately structured into coherent sections, capturing every fact, key point, acronym, example, and explanation. You always begin with a section detailing the article's title, publication date, author, and link (when such information is available). You preserve all the original sections from the article for high fidelity. Aim for a synthesis that is exhaustive, without overlooking any significant information.
 
 Your style is highly formal, logical, and precise. You value consistency and completeness.
 
-Always render all the code snipets delimited by "```code" and "```" verbatim and in full, unless explicitely told otherwise. Most of the time those snipets are in `python`, however apply your reasoning and expertise to tag them with the appropriate programming language.
+Always render all code snippets delimited by "```code" and "```" verbatim and in full, unless explicitly told otherwise. Most of the time, those snippets are in `python`, however, apply your reasoning and expertise to tag them with the appropriate programming language. When indenting Python code, please use an indent of 2 space characters.
 
-Your synthesis is formatted in markdown to maintain clear and organized presentation."""
+Your synthesis is formatted in Markdown to maintain a clear and organized presentation."""
 
+# GPT-4 is inconsistent with the links, (NOTE: those are almost always provided by the "web extractor", but for errors) but ofthen GPT-4 will state that numerous links are provided throughout the article without rendering them!
+# Moreover, you meticulously extract and render all pertinent external links and references (including but not limited to GitHub repositories) cited within the source article.
 
 function make_timed_chat_request(instruct_prompt::String, data::String; kwargs...)
   timeit(
@@ -48,3 +51,41 @@ function save_text(filepath::String, text::String)
     write(fh, text)
   end
 end
+
+function tag_link(tupl::Tuple{String, String})::String
+  link, tag = tupl
+  # @assert length(tag) == 0 "Expect the tag to be empty, when this function is called"
+
+  chunks = split(link, "/")
+  tag = length(tag) == 0 ? chunks[3] : tag
+  (endswith(chunks[end], ".html") || endswith(chunks[end], ".pynb")) &&
+    (tag = string(tag, " - ", replace(chunks[end], ".html" => "")))
+  tag
+end
+
+function handle_duplicate_tag(links::Vector{Tuple{String, String}})::Vector{Tuple{String, String}}
+  tags = Dict{String, Int}()
+  nlinks = Vector{Tuple{String, String}}()
+  for (link, tag) ∈ links
+    if haskey(tags, tag)
+      tags[tag] += 1
+    else
+      tags[tag] = 1
+    end
+  end
+  for (link, tag) ∈ links
+    tags[tag] > 1 && (tag = tag_link((link, tag)))
+    push!(nlinks, (link, tag))
+  end
+  nlinks
+end
+
+# synthesis_links = string(
+# 	join(synthesis, "\n"),
+# 	"\n#### Links:\n",
+# 	join(
+# 		# map(tupl -> string("""  - [$(length(tupl[2]) > 0 ? tupl[2] : split(tupl[1], "/")[3])]""", "($(tupl[1]))"), links),
+# 		map(tupl -> string("""  - [$(length(tupl[2]) > 0 ? tupl[2] : tag_link(tupl))]""", "($(tupl[1]))"), handle_duplicate_tag(links)),
+# 		"\n"
+# 	)
+# )
