f0f7d0c6cd1caadce5900324afdd5b716c8ba428
Pascal P <lacsap_666@yahoo.fr>
2024-01-20 11:06:14 +1300

    abstract support function

--- web-scrapping/support/utils.jl
+++ web-scrapping/support/utils.jl
@@ -1,20 +1,22 @@
+using Dates
+# using OpenAI  # called in pluto
+
+include("../../../../NLP_LLM/Summarize_Papers_with_GPT/support/api_module.jl")
+
 # focus on synthesis rather than summary
-
-# include("../../../NLP_LLM/Summarize_Papers_with_GPT/support/api_module.jl")
-
 const SYS_PROMPT = """```markdown
 You are a smart AI research assistant tasked with analyzing, and synthesizing articles. You are thorough and deliberate in your approach before drafting your answers. You excel at organizing the provided articles into coherent sections, with introductions and conclusions that highlight the main ideas, reasoning, and contributions.
 
 You proceed methodically, step by step, ensuring that the synthesis is accurately structured into coherent sections, capturing every fact, key point, acronym, example, and explanation. You always begin with a section detailing the article's title, publication date, author, and link (when such information is available). You preserve all the original sections from the article for high fidelity. Aim for a synthesis that is exhaustive, without overlooking any significant information.
 
-Your style is highly formal, logical, and precise. You value consistency and completeness.
+Always render all code snippets delimited by "```code" and "```" verbatim and in full, once and only once, unless explicitly told otherwise. Most of the time, those snippets are in `python`, however, apply your reasoning and expertise to tag them with the appropriate programming language. When indenting Python code, please use an indent of 2 space characters.
 
-Always render all code snippets delimited by "```code" and "```" verbatim and in full, unless explicitly told otherwise. Most of the time, those snippets are in `python`, however, apply your reasoning and expertise to tag them with the appropriate programming language. When indenting Python code, please use an indent of 2 space characters.
-
-Your synthesis is formatted in Markdown to maintain a clear and organized presentation."""
+Your style is formal, logical, and precise. You refrain from making comments about your synthesis. You value consistency and completeness.
+You format the synthesis with Markdown."""
 
 # GPT-4 is inconsistent with the links, (NOTE: those are almost always provided by the "web extractor", but for errors) but ofthen GPT-4 will state that numerous links are provided throughout the article without rendering them!
 # Moreover, you meticulously extract and render all pertinent external links and references (including but not limited to GitHub repositories) cited within the source article.
+# Your synthesis is formatted in Markdown to maintain a clear and organized presentation.
 
 function make_timed_chat_request(instruct_prompt::String, data::String; kwargs...)
   timeit(
@@ -26,54 +28,10 @@ function make_timed_chat_request(instruct_prompt::String, data::String; kwargs..
   )
 end
 
-function dft(v_elt::Vector{<: Any})::String
-  """
-  depth first traversal
-  """
-  vtext = String[]
-
-  function _dft(v_elt::Vector{<: Any})
-    for elt ∈ v_elt
-      if hasproperty(elt, :children)
-	_dft(elt.children)
-      elseif hasproperty(elt, :text)
-	push!(vtext, strip(elt.text))
-      end
-    end
-    vtext
-  end
-
-  _dft(v_elt) |> v -> join(v, "\n")
-end
-
 function save_text(filepath::String, text::String)
   open(filepath, "w") do fh
     write(fh, text)
   end
-end
-
-function extract_links(root; selectors=["a"], verbose=true, restrict_to=["github", "LinkedIn"])::Vector{Tuple{String, String}}
-  links = Tuple{String, String}[] # String[]
-  sel_vec = vcat(
-    (eachmatch(Selector(selector), rroot) for selector ∈ selectors)...
-      )
-  for element ∈ sel_vec
-    if hasproperty(element, :attributes)
-      if match(join(restrict_to, "|") |> p -> Regex(p, "i"), element.attributes["href"]) !== nothing
-	# println("1. ", element, "\n")
-	push!(
-	  links,
-	  (element.attributes["href"], dft(element.children))
-	)
-      end
-    end
-  end
-  # remove if link contains "signin?" or "policy..."
-  filter(
-    tupl -> match(r"signin\?|policy\.medium\.com|followers\?|help\.medium|work\-at\-medium|com/about"i,
-                  tupl[1]) === nothing,
-    links
-  )
 end
 
 function tag_link(tupl::Tuple{String, String})::String
@@ -113,11 +71,11 @@ function handle_duplicate_tag(links::Vector{Tuple{String, String}})::Vector{Tupl
 end
 
 # synthesis_links = string(
-# 	join(synthesis, "\n"),
-# 	"\n#### Links:\n",
-# 	join(
-# 		# map(tupl -> string("""  - [$(length(tupl[2]) > 0 ? tupl[2] : split(tupl[1], "/")[3])]""", "($(tupl[1]))"), links),
-# 		map(tupl -> string("""  - [$(length(tupl[2]) > 0 ? tupl[2] : tag_link(tupl))]""", "($(tupl[1]))"), handle_duplicate_tag(links)),
-# 		"\n"
-# 	)
+#       join(synthesis, "\n"),
+#       "\n#### Links:\n",
+#       join(
+#               # map(tupl -> string("""  - [$(length(tupl[2]) > 0 ? tupl[2] : split(tupl[1], "/")[3])]""", "($(tupl[1]))"), links),
+#               map(tupl -> string("""  - [$(length(tupl[2]) > 0 ? tupl[2] : tag_link(tupl))]""", "($(tupl[1]))"), handle_duplicate_tag(links)),
+#               "\n"
+#       )
 # )
---
+++ web-scrapping/support/ws_extractors.jl
@@ -0,0 +1,119 @@
+"""
+Web extraction primitives given a root from which to start and some selectors.
+"""
+# using Cascadia  # Called in Pluto
+# using AbstractTrees
+
+function traverse_article_structure(root; selector="p.pw-post-body-paragraph")
+  for (ix, element) ∈ enumerate(eachmatch(Selector(selector), root))
+    print("ix: $(ix)")
+    if hasproperty(element, :children)
+      println(" current element has $(length(element.children)) child(ren)")
+
+      for child ∈ element.children
+        print("\t", propertynames(child))
+      end
+      println()
+    end
+  end
+end
+
+# PreOrderDFS from AbstractTrees
+# for elem ∈ PreOrderDFS(_root.children[1])  # main
+#   try
+#     if tag(elem) == :p
+#       # println(elem.children[1])
+#       println(AbstractTrees.children(elem)[1])
+#       println("\t", AbstractTrees.children(elem)[2])
+#     end
+#   catch
+#     # Nothing needed here
+#   end
+# end
+
+function extract_content(
+  root;
+  selectors = ["p.nx-mt-6"],
+  detect_code=false,
+  verbose=true
+)::String
+
+  fulltext = String[]
+  sel_vec = _get_selectors(root, selectors)
+  iscode = false
+  for (ix, element) ∈ enumerate(sel_vec)
+    verbose &&  println("$(ix) - [$(element)] /// [$(element.attributes["class"])]")
+
+    # TODO: link `!occursin("fz"...` to class selector for more generality!
+    iscode = detect_code && hasproperty(element, :attributes) &&
+      !occursin("fz", element.attributes["class"])
+
+    if hasproperty(element, :children)
+      text = dft(element.children)
+      text = iscode ? string("```code\n", text, "\n```") : text
+      push!(fulltext, text)
+    end
+
+    iscode = false
+  end
+
+  join(
+    filter(
+      l -> length(strip(l)) > 0,
+      fulltext
+    ),
+    "\n"
+  )
+end
+
+"""
+Extract links fro ma given web page starting at root element
+"""
+function extract_links(
+  root;
+  selectors=["a"], verbose=true,
+  restrict_to=["github", "LinkedIn"],
+  excluding_regex=r"signin\?|policy\.medium\.com|followers\?|help\.medium|work\-at\-medium|com/about"i,
+)::Vector{Tuple{String, String}}
+
+  links = Tuple{String, String}[]
+  sel_vec = _get_selectors(root, selectors)
+  for element ∈ sel_vec
+    if hasproperty(element, :attributes)
+      if match(join(restrict_to, "|") |> p -> Regex(p, "i"), element.attributes["href"]) !== nothing
+        push!(
+          links,
+          (element.attributes["href"], dft(element.children))
+        )
+      end
+    end
+  end
+
+  # remove if link contains "signin?" or "policy..."
+  filter(
+    tupl -> match(excluding_regex, tupl[1]) === nothing,
+    links
+  )
+end
+
+_get_selectors(root, selectors::Vector) = vcat((eachmatch(Selector(selector), root) for selector ∈ selectors)...)
+
+function dft(v_elt::Vector{<: Any})::String
+  """
+  depth first traversal
+  """
+  vtext = String[]
+
+  function _dft(v_elt::Vector{<: Any})
+    for elt ∈ v_elt
+      if hasproperty(elt, :children)
+        _dft(elt.children)
+      elseif hasproperty(elt, :text)
+        push!(vtext, strip(elt.text))
+      end
+    end
+    vtext
+  end
+
+  _dft(v_elt) |> v -> join(v, "\n")
+end
